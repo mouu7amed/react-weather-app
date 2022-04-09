3975274480af56875573fcbe1d9fe32482deca9a
mouu7amed <mouu7amed@gmail.com>
2022-04-10 01:26:39 +0200

    fixing routes issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { Route, Routes } from 'react-router-dom'
 import { Main } from './components/Main/Main';
-import { NotFound } from './components/NotFound/NotFound';
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <Main />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 );
 
